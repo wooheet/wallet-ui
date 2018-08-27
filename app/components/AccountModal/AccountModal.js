f45859660542791961b0bd0cecd4377c21f3a864
tedy <tedy@r2v.io>
2018-08-27 20:56:19 +0900

    Add create account ui

--- app/components/AccountModal/AccountModal.js
+++ app/components/AccountModal/AccountModal.js
@@ -8,6 +8,7 @@ export default class AccountModal extends Component{
   constructor(props) {
     super(props);
     this.state = {
+
     };
 
     this.afterOpenModal = this.afterOpenModal.bind(this);
@@ -23,11 +24,75 @@ export default class AccountModal extends Component{
     this.subtitle.style.color = '#fcfcfc';
     this.subtitle.style.marginLeft ='20px';
     this.subtitle.style.marginTop ='10px';
+    this.subtitle.style.fontStyle= 'italic';
   }
-  render() {
-    const {
 
-    } = this.props;
+  renderImportPassphrase = () => {
+    if(this.props.status === "import"){
+      return (
+        <input
+          className={styles.inputVal}
+          placeholder={"PASSPHRASE"}
+          required
+          maxLength={130}
+          name="importMnemonic"
+          value={this.props.importMnemonic}
+          type={"text"}
+          onChange={this.props.handleInput}
+        />
+      );
+    }else {
+      return null;
+    }
+  };
+
+  btnStatus = () => {
+    if(this.props.status === "import"){
+      return "IMPORT"
+    }else if(this.props.status === "create3"){
+      return "CREATE"
+    }else {
+      return "NEXT"
+    }
+  };
+
+  passwordStrengthMeter = () => {
+    if(this.props.passwordValid === "1"){
+      return (
+        <div className={styles.passwordStrengthMeter1}></div>
+      );
+    }else if(this.props.passwordValid === "2"){
+      return (
+        <div className={styles.passwordStrengthMeter2}></div>
+      );
+    }else if(this.props.passwordValid === "3"){
+      return (
+        <div className={styles.passwordStrengthMeter3}></div>
+      );
+    }else if(this.props.passwordValid === "4"){
+      return (
+        <div className={styles.passwordStrengthMeter4}></div>
+      );
+    }else {
+      return (
+        <div className={styles.passwordStrengthMeter0}></div>
+      );
+    }
+  };
+
+  alertInfo = () => {
+    if(this.props.AlertImportAccount){
+      return <div className={styles.alertInfo}>{this.props.AlertImportAccount}</div>
+    }else if(this.props.AlertImportAccountName){
+      return <div className={styles.alertInfo}>{this.props.AlertImportAccountName}</div>
+    }else if(this.props.AlertImportAccountPass){
+      return <div className={styles.alertInfo}>{this.props.AlertImportAccountPass}</div>
+    }else if(this.props.AlertImportAccountConfirmPass){
+      return <div className={styles.alertInfo}>{this.props.AlertImportAccountConfirmPass}</div>
+    }
+  };
+
+  render() {
     return (
         <Modal
           isOpen={this.props.modalIsOpen}
@@ -36,12 +101,59 @@ export default class AccountModal extends Component{
           className={styles.Modal}
           contentLabel="Account Modal"
           >
-            <h3 ref={subtitle => this.subtitle = subtitle}>{this.props.status === "create" ? "CREATE" : "IMPORT"}</h3>
+            <h3 ref={subtitle => this.subtitle = subtitle}>{this.props.status === "import" ? "IMPORT" : "CREATE"}</h3>
             <div className={styles.line}></div>
             <form>
-              <input />
+              <div className={styles.descriptive}>account name</div>
+              <input
+                className={this.props.AlertImportAccountName ? styles.inputAlertName : styles.inputVal}
+                placeholder={"a descriptive name for the account"}
+                required
+                maxLength={130}
+                name="accountName"
+                value={this.props.accountName}
+                type={"text"}
+                onChange={this.props.handleInput}
+              />
+              <div className={styles.descriptive}>password</div>
+              <input
+                type="password"
+                class="form-control input-lg"
+                id="password"
+                className={this.props.AlertImportAccountPass || this.props.passwordValid === "0" ? styles.inputAlertPass : styles.inputVal}
+                placeholder={"a strong, unique password"}
+                required
+                maxLength={130}
+                name="password"
+                value={this.props.password}
+                type={"password"}
+                onChange={this.props.handleInput}
+              />
+              <div className={styles.descriptive}>password(repeat)</div>
+              <input
+                className={this.props.AlertImportAccountConfirmPass ? styles.inputAlertPassConfirm : styles.inputVal}
+                placeholder={"verify your password"}
+                required
+                maxLength={130}
+                name="passwordConfirm"
+                value={this.props.passwordConfirm}
+                type={"password"}
+                onChange={this.props.handleInput}
+              />
             </form>
-            <button onClick={this.props.closeModal}>close</button>
+            <div className={styles.descriptive}>password strength</div>
+            <div className={styles.passStrength}>
+              {this.passwordStrengthMeter()}
+            </div>
+            <div class="pwstrength_viewport_progress"></div>
+            {this.renderImportPassphrase()}
+            {this.alertInfo()}
+            <div className={styles.btnGroup}>
+              <button className={styles.btn} onClick={this.props.closeModal}>CLOSE</button>
+              <button className={styles.btn} onClick={this.props.createAccount1}>
+                {this.btnStatus()}
+              </button>
+            </div>
         </Modal>
     );
   }
--- app/components/Accounts/Accounts.js
+++ app/components/Accounts/Accounts.js
@@ -11,7 +11,8 @@ import AccountModal from 'components/AccountModal'
 
 const bip38 = require('bip38'),
       HDKey = require("accounts/hdkey"),
-      bip39 = require("bip39");
+      bip39 = require("bip39"),
+      zxcvbn = require('zxcvbn');
 
 type Props = {
   increment: () => void,
@@ -28,46 +29,145 @@ export default class Accounts extends Component<Props> {
     this.state = {
       mnemonic:"",
       modalStatus:"",
-      modalIsOpen: false
+      modalIsOpen: false,
+      accountName:"",
+      password:"",
+      passwordConfirm:"",
+      importMnemonic:"",
+      passwordValid:"",
+      isloading:false,
+      AlertImportAccount:"",
+      AlertImportAccountName:"",
+      AlertImportAccountPass:"",
+      AlertImportAccountConfirmPass:""
     };
 
     this.createModal = this.createModal.bind(this);
     this.importModal = this.importModal.bind(this);
-    this.afterOpenModal = this.afterOpenModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
-
+    this._closeModal = this._closeModal.bind(this);
+    this._handleInput = this._handleInput.bind(this);
+    this.createAccount1 = this.createAccount1.bind(this);
   }
 
-  componentDidMount() {
-
+  componentDidMount = () => {
+    console.log(zxcvbn(this.state.password));
   }
 
-  createModal() {
+  createModal = () => {
     this.setState({
       modalIsOpen: true,
-      modalStatus:"create"
+      modalStatus:"create1"
     });
   }
-  importModal() {
+  importModal = () => {
     this.setState({
       modalIsOpen: true,
       modalStatus:"import"
     });
   }
-  afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    this.subtitle.style.color = '#fcfcfc';
-    this.subtitle.style.marginLeft ='20px';
-    this.subtitle.style.marginTop ='10px';
+
+  _closeModal = () => {
+    this.setState({
+      modalIsOpen: false,
+      accountName:"",
+      password:"",
+      passwordConfirm:"",
+      importMnemonic:"",
+      passwordValid:"0"
+    });
+  }
+  createAccount1 = () =>{
+    if(this.state.accountName===""){
+      this.setState({
+          AlertImportAccountName:"Please enter your account name.",
+          isloading:false
+      });
+    } else if(this.state.password===""){
+      this.setState({
+          AlertImportAccountPass:"Please enter password.",
+          isloading:false
+      });
+    } else if(Number(this.state.passwordValid) < 3){
+      if(zxcvbn(this.state.password).feedback.warning){
+        this.setState({
+          AlertImportAccountPass:zxcvbn(this.state.password).feedback.warning,
+          isloading:false
+        });
+      }else{
+        this.setState({
+          AlertImportAccountPass:"You should satisfy a stronger password.",
+          isloading:false
+        });
+      }
+    } else if(this.state.password !== this.state.passwordConfirm){
+      this.setState({
+          AlertImportAccountPass:"Passwords do not match.",
+          AlertImportAccountConfirmPass:" ",
+          isloading:false
+      });
+    } else if(this.state.passwordConfirm===""){
+      this.setState({
+          AlertImportAccountConfirmPass:"Please enter verity password.",
+          isloading:false
+      });
+    }else {
+      let mnemonic = bip39.generateMnemonic();
+      this.setState(currentState => {
+        return {
+          ...currentState,
+          accountName:this.state.accountName,
+          password:this.state.password,
+          mnemonic:mnemonic,
+          statusModal:"create"
+        };
+      });
+    }
+    setTimeout(() =>{
+      this.setState({
+        AlertImportAccount:"",
+        AlertImportAccountName:"",
+        AlertImportAccountPass:"",
+        AlertImportAccountConfirmPass:""
+      });
+    }, 2000)
   }
 
-  closeModal() {
-    this.setState({modalIsOpen: false});
-  }
 
-  generationMnemonic = () => {
-    let mnemonic = bip39.generateMnemonic();
-    console.log(mnemonic)
+  _handleInput = e => {
+    if(e.target.name === "password"){
+      let pass = e.target.value
+      if(zxcvbn(pass).score ===0){
+        this.setState({
+          passwordValid:"0",
+          AlertImportAccountPass:zxcvbn(pass).feedback.warning,
+        })
+      } else if(zxcvbn(pass).score ===1){
+        this.setState({
+          passwordValid:"1",
+          AlertImportAccountPass:zxcvbn(pass).feedback.warning,
+        })
+      } else if(zxcvbn(pass).score ===2){
+        this.setState({
+          passwordValid:"2",
+          AlertImportAccountPass:zxcvbn(pass).feedback.warning,
+        })
+      } else if(zxcvbn(pass).score ===3){
+        this.setState({
+          passwordValid:"3",
+          AlertImportAccountPass:"",
+        })
+      } else if(zxcvbn(pass).score ===4){
+        this.setState({
+          passwordValid:"4",
+          AlertImportAccountPass:"",
+        })
+      }
+    }
+
+    const { target: { name, value } } = e;
+    this.setState({
+      [name]: value
+    });
   };
 
   render() {
@@ -103,9 +203,19 @@ export default class Accounts extends Component<Props> {
           </button>
           <AccountModal
             modalIsOpen={this.state.modalIsOpen}
-            closeModal={this.closeModal}
-            afterOpenModal={this.afterOpenModal}
+            createAccount1={this.createAccount1}
+            closeModal={this._closeModal}
             status={this.state.modalStatus}
+            handleInput={this._handleInput}
+            accountName={this.state.accountName}
+            password={this.state.password}
+            passwordConfirm={this.state.passwordConfirm}
+            importMnemonic={this.state.importMnemonic}
+            passwordValid={this.state.passwordValid}
+            AlertImportAccount={this.state.AlertImportAccount}
+            AlertImportAccountName={this.state.AlertImportAccountName}
+            AlertImportAccountPass={this.state.AlertImportAccountPass}
+            AlertImportAccountConfirmPass={this.state.AlertImportAccountConfirmPass}
           />
           <div className={styles.logo}>
             <img src={logo} alt="ygg" />
